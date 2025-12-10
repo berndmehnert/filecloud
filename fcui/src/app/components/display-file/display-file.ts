db99634fc6487f7cadfbc5e22daee63369f57540
berndmehnert <bernd.mehnert@protonmail.com>
2025-12-10 22:53:16 +0100

    Fixed files display ..

--- fcui/src/app/components/display-file/display-file.ts
+++ fcui/src/app/components/display-file/display-file.ts
@@ -1,9 +1,10 @@
 import { Component, input } from '@angular/core';
 import { FileMeta } from '../../models/file-meta.model';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-display-file',
-  imports: [],
+  imports: [DatePipe],
   templateUrl: './display-file.html',
   styleUrl: './display-file.css',
   standalone: true,
